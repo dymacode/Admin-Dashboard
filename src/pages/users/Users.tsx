066e347b646bdd6b0b43271499e040d29b24e7b2
dymacode <156077143+dymacode@users.noreply.github.com>
2024-07-08 19:43:22 +0100

    import users.scss corrected

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,7 +1,7 @@
 
 import { GridColDef } from "@mui/x-data-grid";
 import DataTable from "../../components/dataTable/DataTable";
-import "./Users.scss";
+import "./users.scss";
 import { useState } from "react";
 import Add from "../../components/add/Add";
 import { userRows } from "../../data";
